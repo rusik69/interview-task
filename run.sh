b4a370c898b9ae63bfcc072980f2046808b185b0
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2020-05-04 15:26:54 +0200

    commit

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ cd terraform
 ./terraform init
 ./terraform apply -auto-approve
 cd ../ansible
-./ansible-playbook -i inventory.gcp.yml ./wordpress.yml --vault-pass ./vault_pass  -e 'ansible_python_interpreter=/usr/bin/python3'
-./ansible-playbook -i inventory.gcp.yml ./blue.yml -e 'ansible_python_interpreter=/usr/bin/python3'
-./ansible-playbook -i inventory.gcp.yml ./blue.yml -e 'blue_replicas=3' -e 'ansible_python_interpreter=/usr/bin/python3'
-./ansible-playbook -i inventory.gcp.yml ./blue.yml -e 'blue_replicas=3' -e 'blue_index=index-green.html' -e 'ansible_python_interpreter=/usr/bin/python3'
+./ansible-playbook ./wordpress.yml -i inventory.gcp.yml --vault-pass ./vault_pass  -e 'ansible_python_interpreter=/usr/bin/python3'
+./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'blue_replicas=3' -e 'ansible_python_interpreter=/usr/bin/python3'
+./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'blue_replicas=3' -e 'blue_index=index-green.html' -e 'ansible_python_interpreter=/usr/bin/python3'
 cd ..
