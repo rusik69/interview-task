545b1e1d2422e117760de40b960ab75cf27fa55d
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2020-05-05 13:48:33 +0200

    commit

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ cd terraform
 ./terraform init
 ./terraform apply -auto-approve
 cd ../ansible
-./ansible-playbook ./wordpress.yml -i inventory.gcp.yml --vault-pass ./vault_pass  -e 'ansible_python_interpreter=/usr/bin/python3'
-./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'ansible_python_interpreter=/usr/bin/python3'
-./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'blue_replicas=3' -e 'ansible_python_interpreter=/usr/bin/python3'
-./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'blue_replicas=3' -e 'blue_index=index-green.html' -e 'ansible_python_interpreter=/usr/bin/python3'
+./ansible-playbook ./wordpress.yml -i inventory.gcp.yml --vault-pass ./vault_pass
+./ansible-playbook ./blue.yml -i inventory.gcp.yml
+./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'blue_replicas=3'
+./ansible-playbook ./blue.yml -i inventory.gcp.yml -e 'blue_replicas=3' -e 'blue_index=index-green.html'
 cd ..
